35908a925c3094272e362d5c83032bbfee40b270
Swathy Shaji <swathy.shaji@telusinternational.com>
2025-07-16 17:10:09 +0530

    ZCS-17516 : Added new LDAP attribute to accommadate multiple Email address

--- common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
+++ common/src/java/com/zimbra/common/account/ZAttrProvisioning.java
@@ -9517,6 +9517,15 @@ public class ZAttrProvisioning {
     public static final String A_zimbraLicenseNotificationEmail = "zimbraLicenseNotificationEmail";
 
     /**
+     * Email address for receiving Zimbra license notifications for License
+     * Pre Expiry
+     *
+     * @since ZCS 10.1.11
+     */
+    @ZAttr(id=4144)
+    public static final String A_zimbraLicenseNotificationEmailForLicensePreExpiry = "zimbraLicenseNotificationEmailForLicensePreExpiry";
+
+    /**
      * License Pre expiry notification sent details
      *
      * @since ZCS 10.1.10
